1ec85efdf33ef61e563013ef05d7546c4df1e0e4
Vincent Wiart <vincent.wiart@gmail.com>
2024-12-02 18:10:13 +0100

    day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs;
+
+const FILENAME: &str = "input.txt";
+
+fn read_file(left: &mut Vec<i32>, right: &mut Vec<i32>) -> Result<(), String> {
+    let content = fs::read_to_string(FILENAME).expect("Failed to read the file");
+
+    for l in content.lines() {
+        let parsed = sscanf::scanf!(l, "{}   {}", i32, i32);
+        let (a, b) = parsed.expect("Error parsing line");
+        left.push(a);
+        right.push(b);
+    }
+    Ok(())
+}
+
+fn process(left: &mut Vec<i32>, right: &mut Vec<i32>) -> Result<i32, String> {
+    if left.len() != right.len() {
+        return Err("The arrays are not equal".into());
+    }
+    left.sort();
+    right.sort();
+    Ok(left
+        .iter()
+        .zip(right)
+        .fold(0, |acc, (l, r)| acc + (*l - *r).abs()))
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_process() {
+        let mut left: Vec<i32> = vec![3, 4, 2, 1, 3, 3];
+        let mut right: Vec<i32> = vec![4, 3, 5, 3, 9, 3];
+        assert_eq!(super::process(&mut left, &mut right).unwrap(), 11);
+    }
+}
+
+fn main() {
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+    read_file(&mut left, &mut right).expect("unable to read file");
+
+    match process(&mut left, &mut right) {
+        Ok(distance) => println!("Total distance is {}", distance),
+        Err(e) => println!("Error: {}", e),
+    };
+}
